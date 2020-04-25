03d7262a2225f2f01dd4fae1f5d1c44874da591a
Dong Jun Kim <35159798+Immobility@users.noreply.github.com>
2020-04-25 20:37:15 +0000

    Create t2.3.c

---
+++ PLC/t2.3.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+void staticFunction(void) 
+{
+        static int a[1];
+}
+
+void stackFunction(void)
+{
+        int b[1];
+}
+
+void heapFunction(void) 
+{
+        int *array = malloc(sizeof(int[1]));
+        int c[1];
+}
+
+main(void)                                                                                  
+{
+        struct timespec time1, time2;
+        clock_t t1 = clock();
+        for(int i = 0; i < 100000; i++) 
+        {
+                staticFunction();                                                          
+        }
+                      
+        t1 = clock() - t1;
+        double time_taken = ((double)t1/CLOCKS_PER_SEC);
+        printf("it took %f seconds to call staticFunction 100,000 times: \n", time_taken);
+                                     
+        
+        clock_t t2 = clock();
+        for(int i = 0; i < 100000; i++) 
+        { 
+                stackFunction(); 
+        }
+                      
+        t2 = clock() - t2;
+        double time2_taken = ((double)t2/CLOCKS_PER_SEC);
+        printf("it took %f seconds to call staticFunction 100,000 times: \n", time2_taken);
+        
+        
+        clock_t t3 = clock();
+        for(int i = 0; i < 100000; i++) 
+        { 
+                heapFunction();  
+        }
+                      
+        t3 = clock() - t3;
+        double time3_taken = ((double)t3/CLOCKS_PER_SEC);
+        printf("it took %f seconds to call staticFunction 100,000 times: \n", time3_taken);  
+}
