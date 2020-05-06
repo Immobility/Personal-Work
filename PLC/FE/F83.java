a2aebc54cdd77086a1399d94d90bde8b305bd7f4
Dong Jun Kim <35159798+Immobility@users.noreply.github.com>
2020-05-05 22:57:30 -0400

    Update F83.java

--- PLC/FE/F83.java
+++ PLC/FE/F83.java
@@ -1,27 +1,33 @@
-j = -3;
-
-for(i = 0 ; i < 3 ; i++)
-
+public class F83
 {
-    if(j + 2 == 2 || j + 2 == 3)
+	public static void main(String[] args)
 	{
-        j--;
-	}
-	
-    else if( j + 2 == 0 )
-	{
-        j += 2;
-	}
-    else
-	{
-        j = 0;
-	}
-   
-    if( j > 0 )
-	{
-        break;
-	}
-	
-    j = 3 - i;
+		j = -3;
 
+		for(i = 0 ; i < 3 ; i++)
+
+		{
+		    if(j + 2 == 2 || j + 2 == 3)
+			{
+			j--;
+			}
+
+		    else if( j + 2 == 0 )
+			{
+			j += 2;
+			}
+		    else
+			{
+			j = 0;
+			}
+
+		    if( j > 0 )
+			{
+			break;
+			}
+
+		    j = 3 - i;
+
+		}
+	}
 }
