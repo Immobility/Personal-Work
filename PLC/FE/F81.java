5022b42c60c865b7a0e16127432516f8810ba646
Dong Jun Kim <35159798+Immobility@users.noreply.github.com>
2020-05-05 22:56:24 -0400

    Update F81.java

--- PLC/FE/F81.java
+++ PLC/FE/F81.java
@@ -1,22 +1,29 @@
-j = -3;
-
-for(i = 0 ; i < 3 && j < =0 ; i++)
+public class F6
 {
-    switch( j + 2 )
-    {
-        case 3 :
-        case 2 : 
-			j--;
-            continue;
+	public static void main(String[] args)
+	{
 
-        case 0 : 
-			j += 2;
-			continue;
+		j = -3;
 
-        default : j = 0;
+		for(i = 0 ; i < 3 && j < =0 ; i++)
+		{
+		    switch( j + 2 )
+		    {
+			case 3 :
+			case 2 : 
+					j--;
+			    continue;
 
-    }
-	
-    j = 3 - i;
+			case 0 : 
+					j += 2;
+					continue;
 
+			default : j = 0;
+
+		    }
+
+		    j = 3 - i;
+
+		}
+	}
 }
